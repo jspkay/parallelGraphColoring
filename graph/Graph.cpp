64922a61c6f60a3c7e8a1d3c07e6fd30d8777153
Antonio Vespa s284101 <s284101@studenti.polito.it>
2021-08-22 13:19:17 +0200

    habemus CSR funzionante
    CSR + alg sequenziale prima parte

--- graph/Graph.cpp
+++ graph/Graph.cpp
@@ -5,6 +5,8 @@
 #include "Graph.h"
 #include "fstream"
 #include "iostream"
+#include <regex>
+
 using namespace  std;
 
 Graph::Graph() {
@@ -17,20 +19,30 @@ Graph::Graph() {
     if(!fin.good()) {
         cout << "errore lettura" << endl;
     }
-    for(int i=1; i<=V; i++){
-        while(1) {
-            int neighbour;
-            fin >> neighbour;
+    //fstream fout("output.txt", ios::out);
+    string rigaLetta, parsed;
+    smatch m;
+    int neighbour, length;
+    regex code("[0-9]+[ ]*");
+    for(int i=0; i<=V; i++){
+        getline(fin, rigaLetta);
+        while(!rigaLetta.empty()) {
+            regex_search(rigaLetta, m, code);
+            parsed = m.str();
+            length = parsed.size();
+            if(parsed.back()==' ')
+                parsed.pop_back();
+            neighbour = atoi(parsed.c_str());
             std::pair<int, int> edge(i,neighbour);
-            //cout << edge.first << " " << edge.second << endl;
             edges.emplace_back(edge);
-            if(fin.get()=='\n' || fin.eof())
-                break;
+            rigaLetta = rigaLetta.substr(length,rigaLetta.size());
+            //fout << edge.first << "->" <<edge.second << "  ";
         }
+        //fout << "\n";
     }
     fin.close();
-    this->graphCSR = GraphCSR(boost::edges_are_unsorted_multi_pass, std::begin(edges), edges.end(), V+1); //!!!!
-    int cont = 1;
+    this->graphCSR = GraphCSR(boost::edges_are_unsorted_multi_pass, std::begin(edges), edges.end(), V+1);
+    int cont = 0;    //id (informatico) parte da 0, quando stamperai dovrai fare +1
     BGL_FORALL_VERTICES(current_vertex, graphCSR, GraphCSR) {
             graphCSR[current_vertex].id = cont++;
             graphCSR[current_vertex].color = UINT8_MAX;
@@ -43,13 +55,11 @@ Graph::Graph() {
 
 bool Graph::doColoring() {
     BGL_FORALL_VERTICES(current_vertex, graphCSR, GraphCSR) {
+        cout << "current vertex:" << graphCSR[current_vertex].id << ", neigh -> ";
         BGL_FORALL_ADJ(current_vertex, neighbor, graphCSR, GraphCSR){
-            cout << "u:" << graphCSR[current_vertex].id << " ,v:" << graphCSR[neighbor].id << "\n" << endl;
+            cout << graphCSR[neighbor].id << " ";
         }
-        /*for (auto [neighbor, end] = boost::adjacent_vertices(current_vertex, graphCSR); neighbor != end; ++neighbor) {
-
-        }
-         */
+        cout << "\n";
     }
     return false;
 }
