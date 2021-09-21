eec2674c1c60e54dd0603dca79d9879fc3eada4a
Antonio Vespa s284101 <s284101@studenti.polito.it>
2021-09-21 16:17:18 +0200

    updated menu

---
+++ cmake-build-debug/CMakeFiles/GraphColoring.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/antonio_vespa/Documenti/GitHub/parallelGraphColoring/Graph/GraphAdjL.cpp" "/home/antonio_vespa/Documenti/GitHub/parallelGraphColoring/cmake-build-debug/CMakeFiles/GraphColoring.dir/Graph/GraphAdjL.cpp.o"
+  "/home/antonio_vespa/Documenti/GitHub/parallelGraphColoring/Graph/GraphAdjM.cpp" "/home/antonio_vespa/Documenti/GitHub/parallelGraphColoring/cmake-build-debug/CMakeFiles/GraphColoring.dir/Graph/GraphAdjM.cpp.o"
+  "/home/antonio_vespa/Documenti/GitHub/parallelGraphColoring/Graph/GraphCSR.cpp" "/home/antonio_vespa/Documenti/GitHub/parallelGraphColoring/cmake-build-debug/CMakeFiles/GraphColoring.dir/Graph/GraphCSR.cpp.o"
+  "/home/antonio_vespa/Documenti/GitHub/parallelGraphColoring/main.cpp" "/home/antonio_vespa/Documenti/GitHub/parallelGraphColoring/cmake-build-debug/CMakeFiles/GraphColoring.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/antonio_vespa/Documenti/boost_1_77_0"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- main.cpp
+++ main.cpp
@@ -1,13 +1,97 @@
 #include <iostream>
 #include "Graph/Graph.h"
+#include <ctime>
+#define CMDTABLE_ROWS 5
 
+bool continue_loop = true;
+std::string fin_name;
+int int_rep = -1, alg = -1;
 
 void printMenu(){
+    std::cout << "\nGraph Coloring Project menu: \n";
+    std::cout << "[fin] Select input file\n";
+    std::cout << "[ir] Select internal graph representation\n";
+    std::cout << "[a] Select coloring algorithm\n";
+    std::cout << "[cc] Compute colors (not implemented yet!)\n";
+    std::cout << "[ps] Print stats (not implemented yet!)\n";
+    std::cout << "[q] Quit\n" << std::endl;
+}
+
+/*** Command table ***/
+static struct {
+    std::string name;
+    void (*func)();
+} cmdTable[] = {
+        /*** menu ***/
+        {"?", printMenu},
+        /*** operations ***/
+        {"q", [](){ continue_loop = false; }},
+        {"fin", [](){
+                      std::cout << "\nDigit the name: " ;
+                      std::cin >> fin_name;
+                      }},
+        {"ir", [](){
+            std::cout << "\nChoose one of the follower: \n";
+            std::cout << " * 1 - CSR\n";
+            std::cout << " * 2 - Adjacency Matrix\n";
+            std::cout << " * 3 - Adjacency List\n";
+            std::cout << "Digit the number: " ;
+            std::cin >> int_rep;
+        }},
+        {"a", [](){
+            std::cout << "\nChoose one of the follower: \n";
+            std::cout << " * 1 - Jones Plassmann \n";
+            std::cout << " * 2 - Largest Degree\n";
+            std::cout << " * 3 - Smallest Degree\n";
+            std::cout << "Digit the number: " ;
+            std::cin >> alg;
+        }}
+        /*** stats ***/
+};
+
+void credits(){
+   //std::cout << << std::endl;
+   time_t current_time;
+   time(&current_time);
+   std::cout << "**************************************CREDITS*********************************************\n";
+   std::cout << "Graph Coloring Project release 1.3.1, compiled " <<  asctime(localtime(&current_time));
+   std::cout << "Fellows of PDS, Politenico di Torino 2020/2021\n";
+   std::cout << "Professors:\n";
+   std::cout << " * Alessandro Savino\n";
+   std::cout << " * Stefano Quer\n";
+   std::cout << "Students:\n";
+   std::cout << " * Andrea Speziale\n";
+   std::cout << " * Salvatore Pappalardo\n";
+   std::cout << " * Antonio Vespa\n";
+   std::cout << "******************************************************************************************";
+}
+
+void start(){
+    std::string str;
+    std::cout << "\nInitialization Graph Coloring Project...\n";
+    int i;
+    while(continue_loop){
+        std::cout << "\nGraph Coloring Project [? for menu]: ";
+        std::cin >> str;
+        for(i=0; i<CMDTABLE_ROWS; i++) {
+            if(cmdTable[i].name.compare(str)==0) {
+                cmdTable[i].func();
+                break;
+            }
+        }
+        if(i >= CMDTABLE_ROWS)
+            throw std::logic_error("Argument not valid");
+    }
+}
+
+void bootstrap(){
+    credits();
+    start();
     return;
 }
 
 int main(int argc, char* argv[]) {
-    printMenu();
+    //bootstrap();
     const clock_t begin_time = clock();
     asa::GraphAdjL mygraph;
     const clock_t end_construction_time = clock();
